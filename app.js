a17ba7a44e4f84e3e5c83fe223ffad6f54cf3cbf
t2yeah <k450470@yahoo.co.jp>
2020-06-21 07:14:08 +0000

    アルゴリズム改善

--- app.js
+++ app.js
@@ -20,16 +20,4 @@ for (let i = 0; i < 200 ; i++) {
 }
 
 
-// function fib(n) {
-//   if (n === 0) {
-//     return 0;
-//   } else if (n === 1) {
-//     return 1;
-//   }
-//   return fib(n - 1) + fib(n - 2);
-// }
-// const length = 40;
-// for (let i = 0; i <= length; i++) {
-//   console.log(fib(i));
-// }
 
